87a0a13158c784b9847b80361c3624928ec459a6
Anton Vayvod <avayvod@chromium.org>
2015-09-10 22:27:22 +0100

    Added files for real

---
+++ default-request-test.js
@@ -0,0 +1,33 @@
+function printDefaultRequest() {
+  appendMessage('defaultRequest = ' + navigator.presentation.defaultRequest);
+  if (navigator.presentation.defaultRequest) {
+    appendMessage('defaultRequest.onsessionconnect = ' + navigator.presentation.defaultRequest.onsessionconnect + ' === undefined ' + (navigator.presentation.defaultRequest.onsessionconnect === undefined));
+    if (navigator.presentation.defaultRequest.presentationURL)
+      appendMessage('defaultRequest.presentationURL = ' + navigator.presentation.defaultRequest.presentationURL);
+  }
+}
+
+function defaultRequestLoop() {
+  printDefaultRequest();
+  setTimeout(defaultRequestLoop, 10000);
+}
+
+/**
+ * append message to debug message window
+ * @param {string} message A message string
+ */
+function appendMessage(message) {
+  var dw = document.getElementById('debugmessage');
+  dw.innerHTML += '\n' + JSON.stringify(message);
+}
+
+
+function init() {
+  defaultRequestLoop();
+  navigator.presentation.defaultRequest = new PresentationRequest("foo");
+  navigator.presentation.defaultRequest.onsessionconnect = function(e) {
+    console.log("defaultRequest.onsessionconnect");
+  };
+}
+
+
---
+++ event-listener-test.js
@@ -0,0 +1,27 @@
+function print() {
+  appendMessage('screen.orientation.onchange = ' + screen.orientation.onchange);
+}
+
+function loop() {
+  print();
+  setTimeout(loop, 10000);
+}
+
+/**
+ * append message to debug message window
+ * @param {string} message A message string
+ */
+function appendMessage(message) {
+  var dw = document.getElementById('debugmessage');
+  dw.innerHTML += '\n' + JSON.stringify(message);
+}
+
+
+function init() {
+  loop();
+  screen.orientation.onchange = function(e) {
+    console.log("screen.orientation.onchange");
+  };
+}
+
+
